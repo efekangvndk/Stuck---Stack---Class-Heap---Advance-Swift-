b09e0f990334151357ff6b49b94ef96d7f2912c2
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-05-12 12:57:08 +0300

    s

---
+++ main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  Struck & Stack & Class(Heap) "AdvanceSwift"
+//
+//  Created by Efekan Güvendik on 12.05.2024.
+//
+
+import Foundation
+
+var classBuild = MusicianClass(nameInput: "Efe", ageInput: 24, insturemntInput: "Kaval")
+//print(classJames.age)
+//print(classJames.name)
+
+var strcukBuild = MusicianStruck(name: "Efekan", age: 24, Instrument: "Guitar")
+//print(StruckClass.age)
+ 
+//MARK: IMMUTABLE Struck
+/// Eğer let ile oluşturulan bir struck var ise strcuk değişkenleri var bile olsa herhangi bir değişkene değer olarak  ekstra müdahale edilemez
+
+ 
+classBuild.age = 51
+//print(classBuild.age)
+
+strcukBuild.age = 51
+//print(strcukBuild.age)
+
+//MARK: REFERENCE & VALUE
+///Reference : Beklemediğimiz sonucu veren yapı
+///Value :  Beklediğinmiz sonucu veren yapı
+
+let copyOfClassBuild = classBuild
+var copyOfStruckBuild = strcukBuild
+
+print(copyOfClassBuild.age)
+print(copyOfStruckBuild.age)
+
+copyOfClassBuild.age = 20
+copyOfStruckBuild.age = 20
+
+print(copyOfClassBuild.age)
+print(copyOfStruckBuild.age)
+
+print(classBuild.age)
+print(strcukBuild.age)
