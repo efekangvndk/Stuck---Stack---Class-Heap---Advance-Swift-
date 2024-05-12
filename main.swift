b80ab4da2c1b5ec74fe8aaf43d2b16f5488841f4
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-05-12 15:35:59 +0300

    Guard let & if let
    &

--- main.swift
+++ main.swift
@@ -13,11 +13,11 @@ var classBuild = MusicianClass(nameInput: "Efe", ageInput: 24, insturemntInput: 
 
 var strcukBuild = MusicianStruck(name: "Efekan", age: 24, Instrument: "Guitar")
 //print(StruckClass.age)
- 
+
 //MARK: IMMUTABLE Struck
 /// Eğer let ile oluşturulan bir struck var ise strcuk değişkenleri var bile olsa herhangi bir değişkene değer olarak  ekstra müdahale edilemez
 
- 
+
 classBuild.age = 51
 //print(classBuild.age)
 
@@ -30,20 +30,20 @@ strcukBuild.age = 51
 ///Value :  Beklediğinmiz sonucu veren yapı
 
 /*let copyOfClassBuild = classBuild
-var copyOfStruckBuild = strcukBuild
-
-print(copyOfClassBuild.age)
-print(copyOfStruckBuild.age)
-
-copyOfClassBuild.age = 22
-copyOfStruckBuild.age = 22
-
-print(copyOfClassBuild.age)
-print(copyOfStruckBuild.age)
-
-print(classBuild.age)
-print(strcukBuild.age)
-*/
+ var copyOfStruckBuild = strcukBuild
+ 
+ print(copyOfClassBuild.age)
+ print(copyOfStruckBuild.age)
+ 
+ copyOfClassBuild.age = 22
+ copyOfStruckBuild.age = 22
+ 
+ print(copyOfClassBuild.age)
+ print(copyOfStruckBuild.age)
+ 
+ print(classBuild.age)
+ print(strcukBuild.age)
+ */
 
 //Summery : Copy --> Same Object But New Reference --> 1 object 2 reference
 //Value type --> Struck
@@ -51,11 +51,50 @@ print(strcukBuild.age)
 
 //MARK: FUNCTION & MUTATING FUNC
 
-print(classBuild.age)
-classBuild.happyBrithDay()
-print(classBuild.age)
+/*print(classBuild.age)
+ classBuild.happyBrithDay()
+ print(classBuild.age)
+ 
+ print(strcukBuild.age)
+ strcukBuild.happyBirthDay()
+ print(strcukBuild.age)
+ */
 
-print(strcukBuild.age)
-strcukBuild.happyBirthDay()
-print(strcukBuild.age)
+//MARK: TUPLE
+
+/*
+ let myTuple = (1,2,3)
+ print(myTuple.0)
+ 
+ var myTuple2 = (1,2,3,4,5,6,7)
+ myTuple2.1 += 8 //let --> var
+ print(myTuple2.1)
+ 
+ var myTuple3 = (1,2,[10,20,30])
+ print(myTuple3.2[1])
+ 
+ var myArray = [1, 2, [10, 20, 30]] as [Any] // Array kavramı ile deneme üzerine çalışılmalı.
+ print((myArray[2] as? [Int])?[1] ?? "İstediğiniz eleman bulunamadı.")
+ */
+
+//MARK: Guard let & if let
+
+let number = "5"
+
+func convertIntIf(stringInput : String) -> Int{
+    if let myInt = Int(stringInput){
+        return myInt
+    }else {
+        return 0
+    }
+}
+func convertIntGuard(stringInput2 : String) -> Int{
+    guard let  myInt2 = Int(stringInput2) else {
+        return 0
+    }
+    return myInt2
+}
+ 
+print(convertIntGuard(stringInput2: number))
+print(convertIntIf(stringInput: number))
 
