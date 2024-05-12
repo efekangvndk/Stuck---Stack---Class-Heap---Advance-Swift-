8e1ed7475be7c2952f396f0158d7d677c744f8ad
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-05-12 12:56:31 +0300

    Refenece and Value

---
+++ Class/MusicianClass.swift
@@ -0,0 +1,22 @@
+//
+//  MusicianClass.swift
+//  Struck & Stack & Class(Heap) "AdvanceSwift"
+//
+//  Created by Efekan Güvendik on 12.05.2024.
+//
+
+import Foundation
+
+class MusicianClass {
+    var name : String
+    var age : Int
+    var Instument : String
+    
+    init(nameInput: String, ageInput : Int, insturemntInput : String ){
+        self.name = nameInput
+        self.age = ageInput
+        self.Instument = insturemntInput
+         
+        
+    }
+}
---
+++ Struck/MusicianStruck.swift
@@ -0,0 +1,17 @@
+//
+//  MusicianStruck.swift
+//  Struck & Stack & Class(Heap) "AdvanceSwift"
+//
+//  Created by Efekan Güvendik on 12.05.2024.
+//
+
+import Foundation
+
+struct MusicianStruck{
+    
+    var name : String
+    var age : Int
+    var Instrument : String
+    
+}
+
